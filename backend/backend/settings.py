bec44482fb4611dbe81bf1e537577b60ec1fad6e
Maciej Zoltowski <m.zoltowski@gmail.com>
2024-07-22 18:49:15 +0200

    inicjalna konfiguracja po setupie środowiska z mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,14 +106,24 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "contest_platform_database",
+#         "USER": "admin",
+#         "PASSWORD": "admin",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "contest_platform_database",
-        "USER": "admin",
-        "PASSWORD": "admin",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "cpdb",
+        "USER": "cpdb",
+        "PASSWORD": "cpdbpass",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "3306",
     }
 }
 
